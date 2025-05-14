8d3f38bd2459622c9a70b30305410edc59c3ded8
vtalos <v.talos23@gmail.com>
2025-05-14 21:47:43 +0300

    Update input and output dirs

--- create_cat2_devs.py
+++ create_cat2_devs.py
@@ -1,10 +1,23 @@
 from collections import defaultdict
 import json
-with open('rdevs.txt','r') as f:
-    lines = f.readlines()
+import os
+
+# Path to the mappings directory where the JSON file will be saved
+MAPPINGS_DIR = "data/mappings"
+
+# Path to the data directory where the sh output files are located
+DATA_DIR = "outputs"
+
+# Use correct paths to the files
+rdevs_path = os.path.join(DATA_DIR, 'rdevs.txt')
+regular_files_path = os.path.join(DATA_DIR, 'regular_files.txt')
+
 device_nodes = defaultdict(list)
 
 # Uses r_dev to identify the device node
+with open(rdevs_path, 'r') as f:
+    lines = f.readlines()
+
 for line in lines:
     if 'v4l' in line or 'video' in line:
         device_nodes['camera'].append(line.split(' ')[1].replace('\n',''))
@@ -18,10 +31,11 @@ for line in lines:
         device_nodes['bluetooth'].append(line.split(' ')[1].replace('\n',''))
 
 # Uses st_dev and i_node to identify the file
-with open('regular_files.txt','r') as f:
+with open(regular_files_path, 'r') as f:
     lines = f.readlines()
+    
 for line in lines:
-    if 'callogger' in line:
+    if 'callog' in line:
         device_nodes['callogger'].append(f"{line.split(' ')[1]} - {line.split(' ')[2]}".replace('\n',''))
     if 'contacts' in line:
         device_nodes['contacts'].append(f"{line.split(' ')[1]} - {line.split(' ')[2]}".replace('\n',''))
@@ -30,5 +44,9 @@ for line in lines:
     if 'calendar' in line:
         device_nodes['calendar'].append(f"{line.split(' ')[1]} - {line.split(' ')[2]}".replace('\n',''))
 
-with open('cat2_dev.json','w') as f:
+# Save the output to the mappings directory
+output_json = os.path.join(MAPPINGS_DIR, 'cat2_dev.json')
+with open(output_json, 'w') as f:
     json.dump(device_nodes, f, indent=4)
+
+print(f"JSON file created at: {output_json}")
